c4afbc863a1accee9ef5b5945874d7c6b5e726de
scienceopen <scienceopen@users.noreply.github.com>
2015-04-01 13:15:44 -0400

    code reuse

--- demo_hwm93.py
+++ demo_hwm93.py
@@ -7,30 +7,30 @@ Michael Hirsch
 bostonmicrowave.com
 
 Original fortran code by A. E. Hedin
-(retired, formerly at NASA/GSFC)
 from
 ftp://hanna.ccmc.gsfc.nasa.gov/pub/modelweb/atmospheric/hwm93/
 """
 from __future__ import division
-from pandas import Series
 from numpy import arange,asarray
 from matplotlib.pyplot import figure,show
 #
+import sys
+sys.path.append('../msise-00/') #git clone https://github.com/scienceopen/msise-00.git
+from demo_msis import datetime2gtd
 import hwm93
 
-def testhwm93(ip,ap,altkm):
-    stl = ip['sec']/3600 + ip['glon']/15
+def testhwm93(dstr,altkm,glat,glon,f107a,f107,ap):
+    iyd,utsec,stl = datetime2gtd(dstr,glon)
 
     merid = []; zonal=[]
     for a in altkm:
-        m,z = hwm93.gws5(ip['iyd'],ip['sec'],a,ip['glat'],ip['glon'],stl,
-                    ip['f107a'],ip['f107'], ap)
+        m,z = hwm93.gws5(iyd,utsec,a,glat,glon,stl,f107a, f107, ap)
         merid.append(m)
         zonal.append(z)
 
     return asarray(merid),asarray(zonal)
 
-def plothwm(merid,zonal,alt,ip):
+def plothwm(merid,zonal,alt,glat,glon):
     ax = figure().gca()
     ax.plot(merid,alt,color='b',label='merid')
     ax.plot(zonal,alt,color='r',label='zonal')
@@ -38,29 +38,25 @@ def plothwm(merid,zonal,alt,ip):
     ax.set_xlabel('winds [m/s]')
     ax.legend(loc='best')
     ax.grid(True)
-    ax.set_title('Meridional and Zonal winds from HWM93, lat/lon={:.1f},{:.1f}'.format(
-                 ip['glat'],ip['glon']))
+    ax.set_title('Meridional and Zonal winds from HWM93,\n lat/lon={:.1f},{:.1f}'.format(glat,glon))
 
 
 if __name__ == '__main__':
     from argparse import ArgumentParser
     p = ArgumentParser(description='calls HWM93 from Python, a basic demo')
-    p.add_argument('iyd',help='day of year [0-366]',type=int,nargs='?',default=172)
-    p.add_argument('utsec',help='utc second from midnight [0-86400]',type=float,nargs='?',default=29000)
-    p.add_argument('altkm',help='altitude (km) (start,stop,step)',type=float,nargs='?',default=(60,1000,5))
-    p.add_argument('glat',help='geodetic latitude (deg)',type=float,nargs='?',default=60)
-    p.add_argument('glon',help='geodetic longitude (deg)',type=float,nargs='?',default=-70)
-    p.add_argument('F107a',help=' 81 day AVERAGE OF F10.7 FLUX (centered on day DDD)',type=float,nargs='?',default=150)
-    p.add_argument('F107',help='DAILY F10.7 FLUX FOR PREVIOUS DAY',type=float,nargs='?',default=150)
-    p.add_argument('AP',help='daily ap',type=float,nargs='?',default=4)
+    p.add_argument('simtime',help='yyyy-mm-ddTHH:MM:SSZ time of sim',type=str,nargs='?',default='')
+    p.add_argument('-a','--altkm',help='altitude (km) (start,stop,step)',type=float,nargs='+',default=(60,1000,5))
+    p.add_argument('-c','--latlon',help='geodetic latitude (deg)',type=float,default=(65,-148))
+    p.add_argument('f107a',help=' 81 day AVERAGE OF F10.7 FLUX (centered on day DDD)',type=float,nargs='?',default=150)
+    p.add_argument('f107',help='DAILY F10.7 FLUX FOR PREVIOUS DAY',type=float,nargs='?',default=150)
+    p.add_argument('ap',help='daily ap',type=float,nargs='?',default=4)
     p = p.parse_args()
-
-    inp = Series({'iyd':p.iyd, 'sec':p.utsec, 'glat':p.glat, 'glon':p.glon,
-                  'f107a':p.F107a, 'f107':p.F107})
 
     altkm = arange(p.altkm[0],p.altkm[1],p.altkm[2])
 
-    mer,zon = testhwm93(inp,p.AP,altkm)
+    glat,glon = p.latlon
 
-    plothwm(mer,zon,altkm,inp)
+    mer,zon = testhwm93(p.simtime,altkm,glat,glon,p.f107a,p.f107,p.ap)
+
+    plothwm(mer,zon,altkm,glat,glon)
     show()
