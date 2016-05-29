16f11c10f77878987b7e80db2070e77d3a6d6876
michael <nobody@noreply.github.com>
2016-05-29 04:04:50 -0400

    template

--- setup.py
+++ setup.py
@@ -9,20 +9,20 @@ except Exception as e:
 
 from numpy.distutils.core import setup,Extension
 
+ext = Extension(name='hwm93',
+                sources=['fortran/hwm93_sub.for'],
+                f2py_options=['--quiet'])
+
 with open('README.rst','r') as f:
   	long_description = f.read()
 
 setup(name='pyhwm93',
-      version='0.1',
 	  description='HWM93 model',
 	  long_description=long_description,
 	  author='Michael Hirsch',
 	  url='https://github.com/scienceopen/pyhwm93',
       dependency_links = ['https://github.com/scienceopen/gridaurora/tarball/master#egg=gridaurora'],
 	  install_requires=['gridaurora'],
-      packages=['pyhwm93'],
-      ext_modules=[Extension(name='hwm93',
-                sources=['fortran/hwm93_sub.for'],
-                f2py_options=['--quiet'])]
+      ext_modules=[ext]
 	  )
 
